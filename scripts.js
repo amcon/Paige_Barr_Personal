2cb383f3fe4c264ea0d2550a0dc359b1407a37f4
Aaron Conklin <conklin.aaron@gmail.com>
2017-02-22 10:19:48 -0500

    links and h1 color

--- scripts.js
+++ scripts.js
@@ -84,4 +84,43 @@ $(function(){
     setTimeout(reelsModal, 1000);
   });
 
+  $("#news-reel").click(function() {
+    $(".resume").css('opacity', '0');
+    $(".headshots_and_photos").css('opacity', '0');
+    $(".about").css('opacity', '0');
+    $(".reels").css('height', '3500px');
+
+    function reelsModal() {
+      $(".reels").css({
+        opacity: '1',
+        "z-index": '1'
+      });
+      $(".about").css('z-index', '0');
+      $(".resume").css('z-index', '0');
+      $(".headshots_and_photos").css('z-index', '0');
+      $(".footer").css('top', '3700px');
+    };
+    setTimeout(reelsModal, 1000);
+  });
+
+  $("#twitter").click(function(){
+    window.open("https://twitter.com/paigesfunny", "_blank");
+  });
+
+  $("#facebook").click(function(){
+    window.open("https://www.facebook.com/paige.barr.5", "_blank");
+  });
+
+  $("#email").click(function(){
+    window.open("mailto:paigephone123@gmail.com?subject=After visiting your website...", "_blank");
+  });
+
+  $("#vimeo").click(function() {
+    window.open("https://vimeo.com/user9917256", "_blank");
+  });
+
+  $("#youtube").click(function() {
+    window.open("https://www.youtube.com/user/paigemcp", "_blank");
+  });
+
 })
