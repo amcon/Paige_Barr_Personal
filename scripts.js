1b5f24e85fadf629387cd7dfb6e4d813a4f2bfac
Aaron Conklin <conklin.aaron@gmail.com>
2017-02-28 11:02:30 -0500

    state

--- scripts.js
+++ scripts.js
@@ -1,21 +1,32 @@
 $(function(){
 
+  // this ensures that on reload, the scroll position is at the top for
+  // the title page load.
   $("html, body").animate({ scrollTop: 0 }, 0);
 
-  history.pushState({doc: 'title'}, null, "/Paige_Barr_Personal/");
-  console.log(history.state);
 
+
+  // using pushState to set the initial state of the application
+  history.pushState({doc: 'title'}, null, "/Paige_Barr_Personal/");
+
+  // titleDisappear1 & 2 are the functions used to animate the title
+  // screen. It waits a second and a half to activate the fade-out, and
+  // then makes sure the z-index is changed so you can click on the
+  // main site
   function titleDisappear() {
     $(".title-screen").css('opacity', '0');
   };
   setTimeout(titleDisappear, 1500);
-
   function titleDisappear2() {
     history.pushState({doc: 'home'}, null, "/Paige_Barr_Personal/");
     $(".title-screen").css('z-index', '0');
   };
   setTimeout(titleDisappear2, 3000);
 
+
+
+  // whenever the back button is clicked on the browser, we need to inform
+  // the page what elements to load based on the state object it is given.
   window.onpopstate = (event) => {
 
     console.log(history.state);
@@ -89,11 +100,19 @@ $(function(){
   };
 
 
+
+
+
+  // these click functions and setTimeouts are the CSS transitions given to the
+  // single page application to make it seem multi-page and seamless. It causes
+  // the container its on to fade away, and then brings the new view into site,
+  // which activates a fade in.
+
   $("#about").click(function() {
     $(".main-container").css('opacity', '0');
 
     function aboutModal() {
-      history.pushState({doc: 'about'}, null, "/Paige_Barr_Personal/#about");
+      history.pushState({doc: 'about'}, null, "/Paige_Barr_Personal/#/about");
       $(".main-container").css('z-index', '0');
       $(".about").css({
         opacity: '1',
@@ -109,7 +128,7 @@ $(function(){
     $(".main-container").css('opacity', '0');
 
     function resumeModal() {
-       history.pushState({doc: 'resume'}, null, "/Paige_Barr_Personal/#resume");
+       history.pushState({doc: 'resume'}, null, "/Paige_Barr_Personal/#/resume");
        $(".main-container").css('z-index', '0');
        $(".resume").css({
          opacity: '1',
@@ -126,7 +145,7 @@ $(function(){
     $(".main-container").css('opacity', '0');
 
     function headshotModal() {
-      history.pushState({doc: 'headshots'}, null, "/Paige_Barr_Personal/#headshots");
+      history.pushState({doc: 'headshots'}, null, "/Paige_Barr_Personal/#/headshots");
       $(".main-container").css('z-index', '0');
       $(".headshots_and_photos").css({
         opacity: '1',
@@ -142,7 +161,7 @@ $(function(){
     $(".main-container").css('opacity', '0');
 
     function reelsModal() {
-      history.pushState({doc: 'reels'}, null, "/Paige_Barr_Personal/#reels");
+      history.pushState({doc: 'reels'}, null, "/Paige_Barr_Personal/#/reels");
       $(".main-container").css('z-index', '0');
       $(".reels-featured").css({
         opacity: '1',
@@ -154,94 +173,96 @@ $(function(){
 
 
 
-  $(".box-commercials").click(function() {
-    $(".main-container").css('opacity', '0');
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+                $(".box-commercials").click(function() {
+                  $(".main-container").css('opacity', '0');
+                  $("html, body").animate({ scrollTop: 0 }, "slow");
 
-    function commercialsModal() {
-      history.pushState({doc: 'reels-commercials'}, null, "/Paige_Barr_Personal/#reels/#commercials");
-      $(".main-container").css('z-index', '0');
-      $(".reels-commercials").css({
-        opacity: '1',
-        "z-index": '1'
-      });
-    };
-    setTimeout(commercialsModal, 800);
-  });
-
-
-  $(".box-featured").click(function() {
-    $(".main-container").css('opacity', '0');
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-
-    function featuredModal() {
-      history.pushState({doc: 'reels-featured'}, null, "/Paige_Barr_Personal/#reels/#featured");
-      $(".main-container").css('z-index', '0');
-      $(".reels-featured").css({
-        opacity: '1',
-        "z-index": '1'
-      });
-    };
-    setTimeout(featuredModal, 800);
-  });
+                  function commercialsModal() {
+                    history.pushState({doc: 'reels-commercials'}, null, "/Paige_Barr_Personal/#/reels/commercials");
+                    $(".main-container").css('z-index', '0');
+                    $(".reels-commercials").css({
+                      opacity: '1',
+                      "z-index": '1'
+                    });
+                  };
+                  setTimeout(commercialsModal, 800);
+                });
 
 
+                $(".box-featured").click(function() {
+                  $(".main-container").css('opacity', '0');
+                  $("html, body").animate({ scrollTop: 0 }, "slow");
 
-  $(".box-legit").click(function() {
-    $(".main-container").css('opacity', '0');
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-
-    function legitModal() {
-      history.pushState({doc: 'reels-legit'}, null, "/Paige_Barr_Personal/#reels/#legit");
-      $(".main-container").css('z-index', '0');
-      $(".reels-legit").css({
-        opacity: '1',
-        "z-index": '1'
-      });
-    };
-    setTimeout(legitModal, 800);
-  });
+                  function featuredModal() {
+                    history.pushState({doc: 'reels-featured'}, null, "/Paige_Barr_Personal/#/reels/featured");
+                    $(".main-container").css('z-index', '0');
+                    $(".reels-featured").css({
+                      opacity: '1',
+                      "z-index": '1'
+                    });
+                  };
+                  setTimeout(featuredModal, 800);
+                });
 
 
 
-  $(".box-voiceover").click(function() {
-    $(".main-container").css('opacity', '0');
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+                $(".box-legit").click(function() {
+                  $(".main-container").css('opacity', '0');
+                  $("html, body").animate({ scrollTop: 0 }, "slow");
 
-    function voiceModal() {
-      history.pushState({doc: 'reels-voiceover'}, null, "/Paige_Barr_Personal/#reels/#voiceover");
-      $(".main-container").css('z-index', '0');
-      $(".reels-voiceover").css({
-        opacity: '1',
-        "z-index": '1'
-      });
-    };
-    setTimeout(voiceModal, 800);
-  });
-
-
-  $(".box-writing").click(function() {
-    $(".main-container").css('opacity', '0');
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-
-    function writingModal() {
-      history.pushState({doc: 'reels-writing'}, null, "/Paige_Barr_Personal/#reels/#writing");
-      $(".main-container").css('z-index', '0');
-      $(".reels-writing").css({
-        opacity: '1',
-        "z-index": '1'
-      });
-    };
-    setTimeout(writingModal, 800);
-  });
+                  function legitModal() {
+                    history.pushState({doc: 'reels-legit'}, null, "/Paige_Barr_Personal/#/reels/legit");
+                    $(".main-container").css('z-index', '0');
+                    $(".reels-legit").css({
+                      opacity: '1',
+                      "z-index": '1'
+                    });
+                  };
+                  setTimeout(legitModal, 800);
+                });
 
 
+
+                $(".box-voiceover").click(function() {
+                  $(".main-container").css('opacity', '0');
+                  $("html, body").animate({ scrollTop: 0 }, "slow");
+
+                  function voiceModal() {
+                    history.pushState({doc: 'reels-voiceover'}, null, "/Paige_Barr_Personal/#/reels/voiceover");
+                    $(".main-container").css('z-index', '0');
+                    $(".reels-voiceover").css({
+                      opacity: '1',
+                      "z-index": '1'
+                    });
+                  };
+                  setTimeout(voiceModal, 800);
+                });
+
+
+                $(".box-writing").click(function() {
+                  $(".main-container").css('opacity', '0');
+                  $("html, body").animate({ scrollTop: 0 }, "slow");
+
+                  function writingModal() {
+                    history.pushState({doc: 'reels-writing'}, null, "/Paige_Barr_Personal/#/reels/writing");
+                    $(".main-container").css('z-index', '0');
+                    $(".reels-writing").css({
+                      opacity: '1',
+                      "z-index": '1'
+                    });
+                  };
+                  setTimeout(writingModal, 800);
+                });
+
+
+  // like above, these links direct to different pages in the application and act like
+  // our main page redirects.
 
   $(".news-reel").click(function() {
     $(".main-container").css('opacity', '0');
 
     function reelsModal() {
-      history.pushState({doc: 'reels-featured'}, null, "/Paige_Barr_Personal/#reels/#featured");
+      history.pushState({doc: 'reels-featured'}, null, "/Paige_Barr_Personal/#/reels/featured");
       $(".main-container").css('z-index', '0');
       $(".reels-featured").css({
         opacity: '1',
@@ -257,7 +278,7 @@ $(function(){
     $(".main-container").css('opacity', '0');
 
     function reelsModal() {
-      history.pushState({doc: 'reels-legit'}, null, "/Paige_Barr_Personal/#reels/#legit");
+      history.pushState({doc: 'reels-legit'}, null, "/Paige_Barr_Personal/#/reels/legit");
       $(".main-container").css('z-index', '0');
       $(".reels-legit").css({
         opacity: '1',
@@ -269,7 +290,8 @@ $(function(){
 
 
 
-
+  // very simply, on click on these icons, the page opens a new tab to the
+  // corresponding social media page or user's default email.
   $("#twitter").click(function(){
     window.open("https://twitter.com/paigesfunny", "_blank");
   });
