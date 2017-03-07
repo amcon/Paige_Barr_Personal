df43fbe1e6c11cc24b2e7411f871fc16b234a73a
Aaron Conklin <conklin.aaron@gmail.com>
2017-03-07 11:05:48 -0500

    appending/removing videos for speed

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ $(function(){
   // this ensures that on reload, the scroll position is at the top for
   // the title page load.
   $("html, body").animate({ scrollTop: 0 }, 0);
+  window.onload=function(){
+    setTimeout(function(){
+        scrollTo(0,-1);
+    },0);
+  }
 
 
 
